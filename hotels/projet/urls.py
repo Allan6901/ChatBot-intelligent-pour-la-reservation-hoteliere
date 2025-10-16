3daa63f7d70fa52dd675d3fa8585b32a86979489
d21228484 <allan.diny@etu.univ-amu.fr>
2025-10-16 15:12:43 +0200

    Setup Django

--- hotels/projet/urls.py
+++ hotels/projet/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App import views as app_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', app_views.members, name='members'),
 ]
